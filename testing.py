4e1097bcebca1727fb2b6dc37ca38ab941c249a8
Lakshmi653 <lakshmi06dinesh@gmail.com>
2024-11-03 20:08:22 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Hello!I am Lakshmi dinesh")
+st.write("This is my first streamlit app")
+st.write("I'm  student of kite representing AI&DS department")
+st.write("Interested to explore more things in programming")
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background-color: #f0f0f5; 
+    }
+    h1 {
+        color: #2a9d8f;  /* Title color */
+    }
+    .stButton > button {
+        background-color: #264653;  /* Button background */
+        color: white;  /* Button text color */
+        border-radius: 8px;  /* Rounded corners */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
